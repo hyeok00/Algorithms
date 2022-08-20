1f50b5ca4f9bfc4fe87a17381bb5b1c39809d90d
hyeok00 <56781876+hyeok00@users.noreply.github.com>
2022-08-20 13:20:11 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int arr[100];
+void func(int depth, int num, int k)
+{
+	if (depth == num)
+	{
+		for (int i = 0; i < num; i++)
+		{
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 1; i <= k; i++)
+	{
+		int count = 0;
+		for (int j = 0; j < depth; j++)
+		{
+			if (arr[j] >= i)
+			{
+				count = 1;
+				break;
+			}
+		}
+		if (count)
+			continue;
+
+		arr[depth] = i;
+		func(depth + 1, num,k);
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	int N,M;
+	scanf("%d%d", &N, &M);
+	func(0, M,N);
+	return 0;
+}
