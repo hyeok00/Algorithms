3d0babffe6a22f1a04557241f189204bc60fc398
hyeok00 <56781876+hyeok00@users.noreply.github.com>
2022-08-25 17:32:42 +0900

    [Gold IV] Title: 최단경로, Time: 836 ms, Memory: 129584 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,84 +1,83 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.stream.Stream;
 
-/*
- * 다익스트라는 그래프에서 간선에 가중치가 존재할때 사용됨
- * 인접매트릭스, 인접리스트 둘 중하나를 input range에 따라 적절히 사용
- * 입력범위가크므로 리스트를 이용해서 품
- * */
-//시작점에 연결된 간선을 저장할 클래스선언
-class Edge implements Comparable<Edge>{
-	int i, v;
-	public Edge(int i, int v) {
-		this.i = i;
-		this.v = v;
-	}
-	// 우선순위 큐에서 사용시 최솟값 정렬
-	@Override
-	public int compareTo(Edge o) {
-		return this.v - o.v;
-	}	
-}
-public class Main{
-	// 전역적 접근이 필요한 변수 - 인접리스트 list, 결과거리 dist
-	static ArrayList<Edge> list[];
-	static int dist[]; 
-	
-	public static void main(String[] args) throws IOException {
-		// Scanner보다 BuggeredReader가 빠르고 효율적임
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// input 받기
-		int[] in = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray(); 
-		int dot = in[0], line=in[1];
-		int start = Integer.parseInt(br.readLine());
-		
-		// input 받기 - 연결정보 저장
-		list = new ArrayList[dot+1]; // 편의상 0번은 사용하지 않을것이므로 +1
-		for(int i=1;i<list.length;i++) 
-			list[i] = new ArrayList<Edge>();
-		for(int i=0;i<line;i++) {            
-			String [] tt = br.readLine().split(" ");
-	        int a = Integer.parseInt(tt[0]);    //노드1
-	        int b = Integer.parseInt(tt[1]);    //노드2
-	        int w = Integer.parseInt(tt[2]);    // 거리
-	        list[a].add(new Edge(b,w));		
+public class Main {
+	static StringBuilder sb;
+	static int V, E, S;
+	static ArrayList<Node>[] graph;
+	static int[] resultArr;
+
+	static class Node implements Comparable<Node> {
+		int n;
+		int weight;
+
+		public Node(int a, int b) {
+			n = a;
+			weight = b;
 		}
-			
-		
-		// start부터 각 dot으로 가는 최단거리를 저장할 결과배열 dist 선언 및 초기화
-		dist = new int[dot+1]; //input 특성상 0번을 사용하지 않으므로 + 1
-		Arrays.fill(dist, Integer.MAX_VALUE); // 최솟값을 구해야하므로 MAX값으로 초기화
-		dist[start] = 0; // 시작점 거리값
-		
-		// 다익스트라로 start에서 연결된 모른 경로값을 탐색
-		dijkstra(start);
-		
-		// 결과 출력문 (0번 제외)
-		for(int i=1;i<dist.length;i++) {
-			if(dist[i]== Integer.MAX_VALUE)
-				System.out.println("INF");
-			else
-				System.out.println(dist[i]);
+
+		public int compareTo(Node o) {
+			return this.weight - o.weight;
 		}
 	}
-	private static void dijkstra(int s) {
-		PriorityQueue<Edge> pq = new PriorityQueue<Edge>(); //pq를 사용하면 최소값 기준으로 들어가기 때문에 연산이 줄어든다
-		pq.add(new Edge(s,0)); // 탐색 시작점
-		
-		while(!pq.isEmpty()) {
-			Edge now = pq.poll();
-			// 탐색할 점에 연결된 정보기반으로 dist 갱신
-			for(Edge next : list[now.i]) {
-				if(dist[next.i] > now.v + next.v) {
-					dist[next.i] = now.v + next.v;
-					pq.add(new Edge(next.i, dist[next.i]));
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+
+		// 입력부
+		String[] splitedLine = in.readLine().split(" ");
+		V = Integer.parseInt(splitedLine[0]);
+		E = Integer.parseInt(splitedLine[1]);
+		S = Integer.parseInt(in.readLine());
+
+		// 초기화
+		graph = new ArrayList[V + 1];
+		for (int i = 0; i < V + 1; ++i)
+			graph[i] = new ArrayList<Node>();
+
+		resultArr = new int[V + 1];
+		Arrays.fill(resultArr, Integer.MAX_VALUE);
+
+		// 로직
+		for (int i = 0; i < E; ++i) {
+			splitedLine = in.readLine().split(" ");
+			int from = Integer.parseInt(splitedLine[0]);
+			int to = Integer.parseInt(splitedLine[1]);
+			int weight = Integer.parseInt(splitedLine[2]);
+			graph[from].add(new Node(to, weight));
+		}
+
+		resultArr[S] = 0;
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(S, 0));
+
+		while (!pq.isEmpty()) {
+			Node cur = pq.peek();
+			pq.remove();
+
+			// 주변 정점 탐색
+			for (int i = 0; i < graph[cur.n].size(); ++i) {
+				Node next = graph[cur.n].get(i);
+
+				// 최소비용 갱신
+				if (resultArr[next.n] > cur.weight + next.weight) {
+					resultArr[next.n] = cur.weight + next.weight;
+					pq.add(new Node(next.n, resultArr[next.n]));
 				}
-			}			
+			}
 		}
+
+		// 출력부
+		for (int i = 1; i < V + 1; ++i) {
+			if (resultArr[i] == Integer.MAX_VALUE)
+				sb.append("INF").append("\n");
+			else
+				sb.append(resultArr[i]).append("\n");
+		}
+		System.out.println(sb);
 	}
 }
