649a4fb1f1a266d08c21b5cd5f7394f8b542ba75
hyeok00 <56781876+hyeok00@users.noreply.github.com>
2022-11-07 10:46:23 +0900

    [Gold V] Title: 두 용액, Time: 336 ms, Memory: 28324 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+
+		int N = Integer.parseInt(in.readLine());
+		String[] splitedLine = in.readLine().split(" ");
+
+		int[] arr = new int[N];
+		for (int i = 0; i < N; ++i) {
+			arr[i] = Integer.parseInt(splitedLine[i]);
+		}
+		Arrays.sort(arr);
+		int left = 0;
+		int right = N - 1;
+		int first = 0;
+		int second = 0;
+		int sum = Integer.MAX_VALUE;
+		while (!(left == right)) {
+			int value = arr[left] + arr[right];
+			if (Math.abs(value) < Math.abs(sum)) {
+				sum = value;
+				first = arr[left];
+				second = arr[right];
+			}
+			if (value < 0) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		sb.append(first).append(" ").append(second);
+		System.out.println(sb);
+	}
+}
