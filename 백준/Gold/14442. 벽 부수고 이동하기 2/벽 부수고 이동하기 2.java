f77240e3f5a5bb4e6c717285cba06a668139deb3
hyeok00 <56781876+hyeok00@users.noreply.github.com>
2023-06-19 13:07:40 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1636 ms, Memory: 346232 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main {
+
+	static int N, M, K;
+	static String[] splitedLine;
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+
+	static class Pos {
+		int x;
+		int y;
+		int count;
+
+		public Pos(int first, int second, int count) {
+			this.x = first;
+			this.y = second;
+			this.count = count;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		splitedLine = in.readLine().split(" ");
+		N = Integer.parseInt(splitedLine[0]);
+		M = Integer.parseInt(splitedLine[1]);
+		K = Integer.parseInt(splitedLine[2]);
+
+		int[][] map = new int[N][M];
+		for (int i = 0; i < N; ++i) {
+			String inputLine = in.readLine();
+			for (int j = 0; j < M; ++j) {
+				map[i][j] = inputLine.charAt(j) - '0';
+			}
+		}
+
+		int isVisit[][][] = new int[K + 1][N][M];
+		Queue<Pos> q = new ArrayDeque<>();
+		isVisit[0][0][0] = 1;
+
+		q.add(new Pos(0, 0, 0));
+
+		while (!q.isEmpty()) {
+			Pos p = q.poll();
+			if (p.x == N - 1 && p.y == M - 1) {
+				System.out.println(isVisit[p.count][p.x][p.y]);
+				return;
+			}
+
+			for (int i = 0; i < 4; ++i) {
+				int nextX = p.x + dx[i];
+				int nextY = p.y + dy[i];
+				if (isInRange(nextX, nextY)) {
+					if (map[nextX][nextY] == 0 && isVisit[p.count][nextX][nextY] == 0) {
+						isVisit[p.count][nextX][nextY] = isVisit[p.count][p.x][p.y] + 1;
+						q.add(new Pos(nextX, nextY, p.count));
+					} else {
+						// 벽인 경우
+						if (p.count < K && isVisit[p.count + 1][nextX][nextY] == 0) {
+							isVisit[p.count + 1][nextX][nextY] = isVisit[p.count][p.x][p.y] + 1;
+							q.add(new Pos(nextX, nextY, p.count + 1));
+						}
+					}
+				}
+			}
+		}
+		System.out.println(-1);
+
+	}
+
+	private static boolean isInRange(int nextX, int nextY) {
+		if (0 <= nextX && nextX < N && 0 <= nextY && nextY < M)
+			return true;
+		else
+			return false;
+	}
+}
