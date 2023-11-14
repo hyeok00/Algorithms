a3992163be6d673c8b86fcf2d806a6a398229f80
hyeok00 <56781876+hyeok00@users.noreply.github.com>
2023-11-14 10:51:33 +0900

    [Gold III] Title: 색상환, Time: 144 ms, Memory: 18200 KB -BaekjoonHub

---
+++ 백준/Gold/2482. 색상환/색상환.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int N, K;
+	static int[][] dp;
+
+	final static int DIVIDER = 1000000003;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		N = stoi(in.readLine());
+		K = stoi(in.readLine());
+		dp = new int[N + 1][N + 1];
+
+		for (int i = 0; i <= N; ++i) {
+			dp[i][1] = i;
+			dp[i][0] = 1;
+		}
+		for (int i = 4; i <= N; ++i) {
+			for (int j = 1; j <= K; ++j) {
+				dp[i][j] = (dp[i - 2][j - 1] % DIVIDER + dp[i - 1][j] % DIVIDER) % DIVIDER;
+			}
+		}
+		System.out.println(dp[N][K]);
+	}
+
+	private static int stoi(String s) {
+		return Integer.parseInt(s);
+	}
+}
