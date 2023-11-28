4f72eab7e91d791ab0421db7e28945046001e559
hyeok00 <56781876+hyeok00@users.noreply.github.com>
2023-11-28 10:18:19 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[15];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <=n; ++i)
+            for(int j = 0; j < i; ++j)
+                dp[i] += dp[j] * dp[i - j - 1];
+            
+        return dp[n];
+    }
+}
