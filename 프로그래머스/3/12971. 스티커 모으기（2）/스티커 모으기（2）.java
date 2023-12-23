27f2cffef4b68f76275a27a0ed62ce960370edee
hyeok00 <56781876+hyeok00@users.noreply.github.com>
2023-12-23 13:05:26 +0900

    [level 3] Title: 스티커 모으기(2), Time: 20.51 ms, Memory: 58.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class Solution {
+	public int solution(int sticker[]) {
+		int len = sticker.length;
+
+        if(len == 1)
+            return sticker[0];
+        if(len == 2)
+            return Math.max(sticker[0], sticker[1]);
+        
+		int[][] arr = new int[2][len + 1];
+
+		// Step1. 첫번째 집을 포함하는 경우
+		arr[0][0] = sticker[0];
+		arr[0][1] = Math.max(sticker[0], sticker[1]);
+
+		for (int i = 2; i < len - 1; ++i) {
+			if (arr[0][i - 2] > arr[0][i - 1]) {
+				arr[0][i] = arr[0][i - 2] + sticker[i];
+			} else {
+				if (arr[0][i - 1] > arr[0][i - 2] + sticker[i]) {
+					arr[0][i] = arr[0][i - 1];
+				} else {
+					arr[0][i] = arr[0][i - 2] + sticker[i];
+				}
+			}
+		}
+
+		// Step2. 마지막 집을 포함하는 경우
+		arr[1][1] = sticker[1];
+		arr[1][2] = Math.max(sticker[1], sticker[2]);
+
+		for (int i = 3; i < len; ++i) {
+			if (arr[1][i - 2] > arr[1][i - 1]) {
+				arr[1][i] = arr[1][i - 2] + sticker[i];
+			} else {
+				if (arr[1][i - 1] > arr[1][i - 2] + sticker[i]) {
+					arr[1][i] = arr[1][i - 1];
+				} else {
+					arr[1][i] = arr[1][i - 2] + sticker[i];
+				}
+			}
+		}
+
+		// Step3. 위의 두 케이스 중 가장 큰값이 정답이다.
+		return Arrays.stream(arr).flatMapToInt(Arrays::stream).max().getAsInt();
+	}
+}
