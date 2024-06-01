0c1642e87b13069237e195d6d7ea929a22ed3088
hyeok00 <56781876+hyeok00@users.noreply.github.com>
2024-06-01 12:02:56 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
+        char[] cstr = skill.toCharArray();
         for(String s : skill_trees){
             int base = -2;
             boolean flag = true;
-            for(int i = 0; i < skill.length(); ++i){
-                int next = s.indexOf(skill.charAt(i));    
+            for(char c : cstr){
+                int next = s.indexOf(c);    
                 if(next == -1)
                     next = Integer.MAX_VALUE;
                 if(base > next){
