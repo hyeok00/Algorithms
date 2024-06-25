8b634d4f254af68ba79a6e6d0ae8e50a1c64b9f1
hyeok00 <56781876+hyeok00@users.noreply.github.com>
2024-06-25 23:22:21 +0900

    [Gold II] Title: 연료 채우기, Time: 272 ms, Memory: 21176 KB -BaekjoonHub

--- 백준/Gold/1826. 연료 채우기/연료 채우기.java
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.java
@@ -1,9 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -12,9 +8,6 @@ public class Main {
 	static class Pair {
 		int first;
 		int second;
-
-		Pair() {
-		}
 
 		Pair(int first, int second) {
 			this.first = first;
@@ -25,45 +18,37 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader((System.in)));
 		N = stoi(in.readLine());
-		Pair[] list = new Pair[N];
+
+		// 거리순 정렬
+		PriorityQueue<Pair> sortByDist = new PriorityQueue<>((a, b) -> a.first - b.first);
 		for (int i = 0; i < N; ++i) {
 			String[] inputs = in.readLine().split(" ");
-			list[i] = new Pair(stoi(inputs[0]), stoi(inputs[1]));
+			sortByDist.add(new Pair(stoi(inputs[0]), stoi(inputs[1])));
 		}
 		String[] inputs = in.readLine().split(" ");
 		L = stoi(inputs[0]);
 		P = stoi(inputs[1]);
 
-		// 거리순 정렬
-		Arrays.sort(list, (a, b) -> a.first - b.first);
-
 		// 충전량이 많은 순
 		PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.second - a.second);
 
-		int idx = 0;
 		int curPosition = 0;
 		int stopCount = 0;
 
 		while (true) {
 			// 현재 이동 가능한 주유소를 모두 pq에 추가한다.
-			for (int i = idx; i < N; ++i) {
-				if (list[i].first > curPosition + P)
-					break;
-
-				pq.add(list[i]);
-				idx = i + 1;
-			}
+			while (!sortByDist.isEmpty() && sortByDist.peek().first <= curPosition + P)
+				pq.add(sortByDist.poll());
 
 			// 이동 가능한 주유소가 없다.
 			if (pq.isEmpty())
 				break;
 
-			Pair cur = pq.poll();
-
 			// 도착지에 도달 가능하다.
 			if (curPosition + P >= L)
 				break;
-			
+
+			Pair cur = pq.poll();
 			stopCount++;
 			if (curPosition < cur.first) {
 				// 가는 길에 있는경우
